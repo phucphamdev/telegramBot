ce8379b7729c8969c6b996ebec7e34e4f6d53de3
phuc.pham.dev <phuc.pham.dev@gmail.com>
2024-07-16 22:52:12 +0700

    upload code

--- resources/views/layout/partials/sidebar-layout/sidebar/_menu.blade.php
+++ resources/views/layout/partials/sidebar-layout/sidebar/_menu.blade.php
@@ -64,72 +64,72 @@
 					</div>
 					<!--end:Menu item-->
 					<!--begin:Menu item-->
-					<div class="menu-item">
-						<!--begin:Menu link-->
-						<a class="menu-link {{ request()->routeIs('user-management.roles.*') ? 'active' : '' }}" href="{{ route('user-management.roles.index') }}">
-							<span class="menu-bullet">
-								<span class="bullet bullet-dot"></span>
-							</span>
-							<span class="menu-title">Roles</span>
-						</a>
-						<!--end:Menu link-->
-					</div>
+{{--					<div class="menu-item">--}}
+{{--						<!--begin:Menu link-->--}}
+{{--						<a class="menu-link {{ request()->routeIs('user-management.roles.*') ? 'active' : '' }}" href="{{ route('user-management.roles.index') }}">--}}
+{{--							<span class="menu-bullet">--}}
+{{--								<span class="bullet bullet-dot"></span>--}}
+{{--							</span>--}}
+{{--							<span class="menu-title">Roles</span>--}}
+{{--						</a>--}}
+{{--						<!--end:Menu link-->--}}
+{{--					</div>--}}
 					<!--end:Menu item-->
 					<!--begin:Menu item-->
-					<div class="menu-item">
-						<!--begin:Menu link-->
-						<a class="menu-link {{ request()->routeIs('user-management.permissions.*') ? 'active' : '' }}" href="{{ route('user-management.permissions.index') }}">
-							<span class="menu-bullet">
-								<span class="bullet bullet-dot"></span>
-							</span>
-							<span class="menu-title">Permissions</span>
-						</a>
-						<!--end:Menu link-->
-					</div>
+{{--					<div class="menu-item">--}}
+{{--						<!--begin:Menu link-->--}}
+{{--						<a class="menu-link {{ request()->routeIs('user-management.permissions.*') ? 'active' : '' }}" href="{{ route('user-management.permissions.index') }}">--}}
+{{--							<span class="menu-bullet">--}}
+{{--								<span class="bullet bullet-dot"></span>--}}
+{{--							</span>--}}
+{{--							<span class="menu-title">Permissions</span>--}}
+{{--						</a>--}}
+{{--						<!--end:Menu link-->--}}
+{{--					</div>--}}
 					<!--end:Menu item-->
 				</div>
 				<!--end:Menu sub-->
 			</div>
 			<!--end:Menu item-->
-			<!--begin:Menu item-->
-			<div class="menu-item pt-5">
-				<!--begin:Menu content-->
-				<div class="menu-content">
-					<span class="menu-heading fw-bold text-uppercase fs-7">Help</span>
-				</div>
-				<!--end:Menu content-->
-			</div>
-			<!--end:Menu item-->
-			<!--begin:Menu item-->
-			<div class="menu-item">
-				<!--begin:Menu link-->
-				<a class="menu-link" href="https://preview.keenthemes.com/html/metronic/docs/base/utilities" target="_blank">
-					<span class="menu-icon">{!! getIcon('rocket', 'fs-2') !!}</span>
-					<span class="menu-title">Components</span>
-				</a>
-				<!--end:Menu link-->
-			</div>
-			<!--end:Menu item-->
-			<!--begin:Menu item-->
-			<div class="menu-item">
-				<!--begin:Menu link-->
-				<a class="menu-link" href="https://preview.keenthemes.com/laravel/metronic/docs" target="_blank">
-					<span class="menu-icon">{!! getIcon('abstract-26', 'fs-2') !!}</span>
-					<span class="menu-title">Documentation</span>
-				</a>
-				<!--end:Menu link-->
-			</div>
-			<!--end:Menu item-->
-			<!--begin:Menu item-->
-			<div class="menu-item">
-				<!--begin:Menu link-->
-				<a class="menu-link" href="https://preview.keenthemes.com/laravel/metronic/docs/changelog" target="_blank">
-					<span class="menu-icon">{!! getIcon('code', 'fs-2') !!}</span>
-					<span class="menu-title">Changelog v8.2.5</span>
-				</a>
-				<!--end:Menu link-->
-			</div>
-			<!--end:Menu item-->
+{{--			<!--begin:Menu item-->--}}
+{{--			<div class="menu-item pt-5">--}}
+{{--				<!--begin:Menu content-->--}}
+{{--				<div class="menu-content">--}}
+{{--					<span class="menu-heading fw-bold text-uppercase fs-7">Help</span>--}}
+{{--				</div>--}}
+{{--				<!--end:Menu content-->--}}
+{{--			</div>--}}
+{{--			<!--end:Menu item-->--}}
+{{--			<!--begin:Menu item-->--}}
+{{--			<div class="menu-item">--}}
+{{--				<!--begin:Menu link-->--}}
+{{--				<a class="menu-link" href="https://preview.keenthemes.com/html/metronic/docs/base/utilities" target="_blank">--}}
+{{--					<span class="menu-icon">{!! getIcon('rocket', 'fs-2') !!}</span>--}}
+{{--					<span class="menu-title">Components</span>--}}
+{{--				</a>--}}
+{{--				<!--end:Menu link-->--}}
+{{--			</div>--}}
+{{--			<!--end:Menu item-->--}}
+{{--			<!--begin:Menu item-->--}}
+{{--			<div class="menu-item">--}}
+{{--				<!--begin:Menu link-->--}}
+{{--				<a class="menu-link" href="https://preview.keenthemes.com/laravel/metronic/docs" target="_blank">--}}
+{{--					<span class="menu-icon">{!! getIcon('abstract-26', 'fs-2') !!}</span>--}}
+{{--					<span class="menu-title">Documentation</span>--}}
+{{--				</a>--}}
+{{--				<!--end:Menu link-->--}}
+{{--			</div>--}}
+{{--			<!--end:Menu item-->--}}
+{{--			<!--begin:Menu item-->--}}
+{{--			<div class="menu-item">--}}
+{{--				<!--begin:Menu link-->--}}
+{{--				<a class="menu-link" href="https://preview.keenthemes.com/laravel/metronic/docs/changelog" target="_blank">--}}
+{{--					<span class="menu-icon">{!! getIcon('code', 'fs-2') !!}</span>--}}
+{{--					<span class="menu-title">Changelog v8.2.5</span>--}}
+{{--				</a>--}}
+{{--				<!--end:Menu link-->--}}
+{{--			</div>--}}
+{{--			<!--end:Menu item-->--}}
 		</div>
 		<!--end::Menu-->
 	</div>
