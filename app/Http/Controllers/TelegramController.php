55561875a73408e3080bab68e12ead71caeac77c
phuc.pham.dev <phuc.pham.dev@gmail.com>
2024-07-17 11:10:00 +0700

    upload code 5

--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -3,16 +3,53 @@
 
 
 	use Illuminate\Http\Request;
-	use Telegram\Bot\Laravel\Facades\Telegram;
+    use Illuminate\Support\Facades\DB;
+    use Telegram\Bot\Laravel\Facades\Telegram;
 
 	class TelegramController extends Controller
 	{
+
+        public function handle(Request $request)
+        {
+            // Extract data from the request
+            $data = $request->all();
+
+            // Validate the request
+            $validatedData = $request->validate([
+                'chat_id' => 'required|integer',
+                'text' => 'required|string',
+                'time' => 'required|date',
+                'label' => 'required|string',
+                'price' => 'required|numeric'
+            ]);
+
+            // Save data to the database
+            $data = [
+                'chat_id' => $validatedData['chat_id'],
+                'text' => $validatedData['text'],
+                'time' => $validatedData['time'],
+                'label' => $validatedData['label'],
+                'price' => $validatedData['price']
+            ];
+
+            DB::table('nhap')->insert($data);
+
+            return response()->json(['message' => 'Data saved successfully'], 200);
+        }
+
+
         public function webhook(Request $request)
         {
             $update = Telegram::commandsHandler(true);
 
             $chatId = $update->getMessage()->getChat()->getId();
             $message = $update->getMessage()->getText();
+
+            Telegram::sendMessage([
+                'chat_id' => env('TELEGRAM_CHANNEL_ID', ''),
+                'parse_mode' => 'HTML',
+                'text' => $message
+            ]);
 
             if ($message == '/start') {
                 Telegram::sendMessage([
